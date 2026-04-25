bbfe829a8c099cc92f16a7713a39c8c489127275
Henri Hörkkö <henri.h1979@gmail.com>
2026-04-26 00:07:12 +0200

    Add run analysis script

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+# Kör hela analysen från början till slut
+
+source("scripts/01_data_understanding.R")
+source("scripts/02_data_cleaning.R")
+source("scripts/03_descriptive_analysis.R")
+source("scripts/04_regression_models.R")
+source("scripts/05_model_comparison.R")
